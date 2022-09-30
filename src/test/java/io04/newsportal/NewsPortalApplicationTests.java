c2cb91828f45d0a516059fede21e741a65accb85
Vodzinskiy <roma.vodzinskiy@gmail.com>
2022-09-30 19:47:23 +0300

    create project

---
+++ src/test/java/io04/newsportal/NewsPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package io04.newsportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
