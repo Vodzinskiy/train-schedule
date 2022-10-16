5cf487e2a5675b87161618eaa849048b41372544
Voznytsia <dmitryvoznitsa@gmail.com>
2022-10-16 12:42:02 +0300

    added and connected admin page to java

--- src/main/java/io04/trainschedule/controllers/HomeController.java
+++ src/main/java/io04/trainschedule/controllers/HomeController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String mainHtml(){
-        return "index.html";
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String adminHtml(){
+        return "admin";
     }
 }
