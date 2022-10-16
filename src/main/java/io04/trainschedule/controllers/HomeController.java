fa4a50f95a44a01ab1d7a4d8772c4cbb7ea9e5b3
Voznytsia <dmitryvoznitsa@gmail.com>
2022-10-16 12:35:12 +0300

    Connected html files to java

--- src/main/java/io04/trainschedule/controllers/HomeController.java
+++ src/main/java/io04/trainschedule/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package io04.trainschedule.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+    @GetMapping("/")
+    public String mainHtml(){
+        return "index.html";
+    }
 }
