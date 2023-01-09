1940ca766a88d5f689ccc136c62ccc1298534d2b
Arsen0109 <balasarseniy777@gmail.com>
2023-01-09 11:53:14 +0200

    Open API documentation for MainRestController

--- src/main/java/io04/trainschedule/rest/MainRestController.java
+++ src/main/java/io04/trainschedule/rest/MainRestController.java
@@ -1,5 +1,9 @@
 package io04.trainschedule.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import io04.trainschedule.models.DataTime;
 import io04.trainschedule.models.Train;
 import io04.trainschedule.repositories.TrainRepository;
@@ -14,7 +18,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Tag(
+        name = "Trains",
+        description = "Demonstration of main logic of application as REST ful web service"
+)
 @RestController
 @RequestMapping("/trains_rest")
 public class MainRestController {
@@ -27,7 +34,9 @@ public class MainRestController {
 
     @Autowired
     StationService stationService;
-
+    @Operation(summary = "Get suitable train", description = "Getting suitable train by station of departure, station of arrival and date of departure used ad request params")
+    @ApiResponse(responseCode = "200", description = "Everything is OK")
+    @ApiResponse(responseCode = "404", description = "Error bad request", content = @Content)
     @GetMapping
     public ResponseEntity<?> getTrains(@RequestParam String stationOfDeparture, @RequestParam String stationOfArrival, @RequestParam String dateTime){
         if (stationOfDeparture == null || stationOfArrival == null || dateTime == null){
